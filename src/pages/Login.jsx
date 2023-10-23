feff5c08875df70efc4d0b1d28f24ec435d40ec6
namrathashivaHS <47343765+namrathashivaHS@users.noreply.github.com>
2023-10-23 21:24:50 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -34,7 +34,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:8000/login",
+        "https://hvs-backend.onrender.com/login",
         {
           ...inputValue,
         },
