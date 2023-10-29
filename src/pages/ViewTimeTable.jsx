7d28a4162dc3f7c8605f0fadcd8fcab2b633e8cd
namrathashivaHS <47343765+namrathashivaHS@users.noreply.github.com>
2023-10-29 17:08:47 +0530

    Update ViewTimeTable.jsx

--- src/pages/ViewTimeTable.jsx
+++ src/pages/ViewTimeTable.jsx
@@ -80,9 +80,10 @@ function ViewTimeTable() {
 
   const [user, setChildData] = useState('');
   const receiveDataFromChild = (user) => {
+     const token = sessionStorage.getItem('jwt');
     // Update the parent component's state with the received data
     setChildData(user);
-    if(cookies.jwt && (user.role=="Admin")){
+    if(token && (user.role=="Admin")){
         navigate("/admin/timeTable");
       }else{
         navigate("/login");
