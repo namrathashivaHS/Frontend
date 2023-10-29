53b86a86a6f077521e15299a4d7f30b0aa35acde
namrathashivaHS <47343765+namrathashivaHS@users.noreply.github.com>
2023-10-29 17:08:13 +0530

    Update ViewStudent.jsx

--- src/pages/ViewStudent.jsx
+++ src/pages/ViewStudent.jsx
@@ -17,11 +17,12 @@ function ViewStudent() {
     const navigate=useNavigate();
     const [cookies,removeCookie] = useCookies([]);
     const receiveDataFromChild = async(user) => {
+         const token = sessionStorage.getItem('jwt');
         // Update the parent component's state with the received data
         setChildData(user);
         navigate('/admin/viewStudent');
         
-    if(cookies.jwt && user.role=="Admin"){
+    if(token && user.role=="Admin"){
         try{
                 const { data } = await axios.get('https://hvs-backend.onrender.com/admin/viewStudent',
                 {
